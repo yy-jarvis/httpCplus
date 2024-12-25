dc4c7593bbd0361abc0188ea0dcf0a48eb4424f0
JayAI <jyanyong56@163.com>
2024-12-25 11:09:48 +0800

    add http客户端

---
+++ client/HttpRequest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jyany on 24-12-25.
+//
+
+#include "HttpRequest.hpp"
---
+++ client/HttpRequest.hpp
@@ -0,0 +1,16 @@
+//
+// Created by jyany on 24-12-25.
+//
+
+#ifndef HTTPREQUEST_HPP
+#define HTTPREQUEST_HPP
+
+
+
+class HttpRequest {
+
+};
+
+
+
+#endif //HTTPREQUEST_HPP
